ef46895b88cb939ae01e8e0d1a2ae902f9ed32c6
Jailson Tenorio do Nascimento <jailsontenorio@gmail.com>
2022-07-24 13:36:42 -0300

    Add correction suggestion using list of allowed files

--- 00_directory_traversal/app.js
+++ 00_directory_traversal/app.js
@@ -5,9 +5,19 @@ const app = express()
 const folder_download = './files'
 const query_string = 'file'
 
+//TODO Fix
+function valide_filename(filename){
+    allow_filename = await fs.readdirSync(folder_download)
+    if (! allow_filename.includes(filename)){
+        throw new Error('Arquivo inválido')
+    }
+}
+
 app.get('/download', function(req, res, next){
-    const filename = req.query[query_string]
     //TODO vulnerable to Directory traversal
+    const filename = req.query[query_string]
+    //TODO Fix
+    // const filename = valide_filename(req.query[query_string])
     const file = fs.readFileSync(`${folder_download}/${filename}`)
     res.send(file)    
 })
